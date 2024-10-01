dfa9eaf3d34be6a6d83829b8edd296feb9a7c59b
baku-syoubai <mozhangmei@gmail.com>
2024-10-01 11:37:34 +0900

    Create front-page.php
    upload front-page

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<main>
+    <section>
+        KV
+    </section>
+    <section>about me</section>
+    <section> search</section>
+    <section> ranking</section>
+    <section> column</section>
+    <section> news</section>
+</main>
+<?php get_footer(); ?>
