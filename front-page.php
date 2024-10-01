5f4eedb9b5de06225479cb841aea457098f9e20f
baku-syoubai <mozhangmei@gmail.com>
2024-10-01 15:22:12 +0900

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -12,13 +12,17 @@
         $args = [
             'post_type' => 'page',
             'post__in' => ['47'],
+            // 'post__in' => ['174'],
         ];
         $the_query = new WP_Query($args);
         if ($the_query->have_posts()):
             while ($the_query->have_posts()): $the_query->the_post();
         ?>
                 <div>
-                    <?php echo the_excerpt(); ?>
+                    <?php echo the_excerpt(); ?><!-- 抜粋 -->
+                </div>
+                <div>
+                    <?php the_post_thumbnail(); ?><!-- アイキャッチ画像 -->
                 </div>
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
