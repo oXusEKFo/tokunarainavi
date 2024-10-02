4fe4632b20707329a0b07d820c7402d9a1ec3f16
baku-syoubai <mozhangmei@gmail.com>
2024-10-02 09:53:43 +0900

    update
    update

--- front-page.php
+++ front-page.php
@@ -8,11 +8,12 @@
 
     <!-- about me -->
     <section>
+        <h1>about me </h1>
         <?php
         $args = [
             'post_type' => 'page',
-            // 'post__in' => ['47'],
-            'post__in' => ['174'],
+
+            'post__in' => ['174'], // サーバ環境用
         ];
         $the_query = new WP_Query($args);
         if ($the_query->have_posts()):
@@ -22,7 +23,7 @@
                     <?php echo the_excerpt(); ?><!-- 抜粋 -->
                 </div>
                 <div>
-                    <?php the_post_thumbnail(); ?><!-- アイキャッチ画像 -->
+                    <?php the_post_thumbnail('medium'); ?><!-- アイキャッチ画像 -->
                 </div>
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
@@ -32,7 +33,23 @@
 
     <section> search</section>
     <section> ranking</section>
-    <section> column</section>
+
+    <!-- column -->
+    <section>
+        <h1>column</h1>
+        <?php
+        $args = [
+            'post_type' => 'column',
+        ];
+        $column_query = new WP_query($args);
+        if ($column_query->have_posts()):
+        ?>
+            <?php while ($column_query->have_posts()): $column_query->the_post(); ?>
+                <?php get_template_part('template-parts/loop', 'column'); ?>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        <?php endif ?>
+    </section>
     <section> news</section>
 </main>
 <?php get_footer(); ?>
