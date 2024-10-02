205ef807fd8a6f1c86800ce4f4f49293a0a4cb48
baku-syoubai <mozhangmei@gmail.com>
2024-10-02 14:54:29 +0900

    Update functions.php

--- front-page.php
+++ front-page.php
@@ -23,16 +23,27 @@
                     <?php echo the_excerpt(); ?><!-- 抜粋 -->
                 </div>
                 <div>
-                    <?php the_post_thumbnail('medium'); ?><!-- アイキャッチ画像 -->
+                    <?php the_post_thumbnail(); ?><!-- アイキャッチ画像 -->
                 </div>
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
         <?php endif ?>
     </section>
 
-
-    <section> search</section>
-    <section> ranking</section>
+    <!-- search -->
+    <section>
+        <h1>search</h1>
+        <h2>エリア</h2>
+        <!-- <?php get_template_part('template-parts/search', 'area'); ?> -->
+        <h2>年齢</h2>
+        <!-- <?php get_template_part('template-parts/search', 'age'); ?> -->
+        <h2>ジャンル</h2>
+        <!-- <?php get_template_part('template-parts/search', 'category'); ?> -->
+    </section>
+    <section>
+        <h1>ranking</h1>
+        <a href="<?php echo home_url('/fushion'); ?>" class="">もっと見る</a>
+    </section>
 
     <!-- column -->
     <section>
@@ -49,7 +60,26 @@
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
         <?php endif ?>
+
+        <a href="<?php echo home_url('/column'); ?>" class="">もっと見る</a>
     </section>
-    <section> news</section>
+
+    <section>
+        <h1>news</h1>
+        <?php
+        $args = [
+            'post_type' => 'post',
+        ];
+        $the_query = new WP_Query($args);
+        if ($the_query->have_posts()):
+            while ($the_query->have_posts()): $the_query->the_post();
+        ?>
+                <?php get_template_part('template-parts/loop', 'news'); ?>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        <?php endif ?>
+
+        <a href="<?php echo home_url('/category'); ?>" class="">もっと見る</a>
+    </section>
 </main>
 <?php get_footer(); ?>
