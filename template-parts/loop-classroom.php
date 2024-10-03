d433835e3c3451f626b65f1f1f308c4d32cff06c
baku-syoubai <mozhangmei@gmail.com>
2024-10-03 09:48:44 +0900

    更新

--- template-parts/loop-classroom.php
+++ template-parts/loop-classroom.php
@@ -10,24 +10,13 @@
             <?php endif; ?>
         </div>
 
-        <!-- category-name -->
-        <!-- <?php
-                $categories = get_the_category();
-                if ($categories):
-                ?>
-            <div class="card_label">
-                <?php foreach ($categories as $category): ?>
-                    <span class="label label-black"><?php echo $category->name; ?></span>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?> -->
-
-
         <div class="card_body">
 
             <h2 class="card_title"><?php the_title(); ?></h2>
-            <p> <?php echo the_excerpt(); ?></p> <!-- 抜粋 -->
-            <time datetime="<?php the_time('y-m-d'); ?>"><?php the_time('Y年m月d日') ?> </time>
+            <?php
+            echo get_field('address');
+            ?>
+
         </div>
     </a>
 </section>
