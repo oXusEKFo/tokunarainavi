bb504e2e94a5b8d868b3dbca6899092bd6475d80
synki0628 <move557188@gmail.com>
2024-10-04 15:19:13 +0900

    追加
    追加しました
    
    鳥井

--- 404.php
+++ 404.php
@@ -9,20 +9,13 @@
         </div>
     </section>
 
-    <section class="section">
-        <div class="section_inner">
-            <div class="section_header">
-                <h2 class="heading heading-primary"><span>エラー</span>404 Not Found</h2>
-            </div>
 
-
-            <div class="content">
-                <p>お探しのページが見つかりませんでした。</p>
-                <p>申し訳ございませんが、<a href="<?php echo home_url(); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
-            </div>
-        </div>
-        </div>
-    </section>
+    <div class="container">
+        <h1エラー< /h1>
+            <div class="error_code">404 Not Found</div>
+            <p>お探しのページが見つかりませんでした。</p>
+            <p>申し訳ございませんが、<a href="<?php echo home_url(); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
+    </div>
 </main>
 
 <?php get_footer(); ?>
