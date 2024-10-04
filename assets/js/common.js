6ec9fe693fa7acad7f9362a090478dcb32cc77ea
morimoto-web17 <youchan03292000@gmail.com>
2024-10-04 15:28:36 +0900

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -24,4 +24,13 @@ function toggleMenu() {
             });
         });
     });
+
+// common.js
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
 }
