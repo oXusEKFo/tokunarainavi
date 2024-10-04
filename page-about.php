cbc765811aab56c1b55590a82f898f5108ef044f
synki0628 <move557188@gmail.com>
2024-10-04 17:15:46 +0900

    変更しました。
    変更しました。

--- page-about.php
+++ page-about.php
@@ -4,27 +4,6 @@
     <?php while (have_posts()) : the_post(); ?>
 
         <main>
-<<<<<<< Updated upstream
-            <section class="container_bread-crumb">
-                <div class="bread-crumb">
-                    <p><a href="<?php echo home_url(); ?>">TOP</a> &gt;
-                        <span class="under-line">このサイトについて</span>
-                    </p>
-                </div>
-            </section>
-            <div class="inner_main">
-
-                <!-- 本文記述 -->
-                <section class="wrap_about">
-                    <h1 class="title_about">
-                        <?php the_title(); ?>
-                    </h1>
-                    <div class="wrap_description">
-                        <div class="description_about">
-                            <div class="note_about">
-                                <?php the_content(); ?>
-                            </div>
-=======
 
             <section class="container_bread-crumb">
                 <div class="bread-crumb">
@@ -42,7 +21,6 @@
                     <div class="description_about">
                         <div class="note_about">
                             <?php the_content(); ?>
->>>>>>> Stashed changes
                         </div>
                     </div>
 
