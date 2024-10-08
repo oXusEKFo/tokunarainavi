e27cde36f54b40bd7c7d31c411202723fa4c7513
baku-syoubai <mozhangmei@gmail.com>
2024-10-08 13:55:10 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,20 +1,34 @@
-<footer class="footer">
-
-    <!-- footer-menu -->
-    <?php
-    $args = [
-        'menu' => 'footer_nav', //管理画面で作成したメニューの名前
-        'menu_class' => '', //メニューを構成するulタグのクラス名
-        'container' => false, //<ul>タグを囲んでいるdivタグを消除
-    ];
-    wp_nav_menu($args);
-    ?>
-
-</footer>
-
-<div class="pageTop js-toTop">
-    <a href="#"><i class="fas fa-arrow-up"></i><span>TOP PAGE</span></a>
+<!-- Topへ戻るボタン -->
+<div class="back-to-top">
+    <a href="#">
+        <span>BACK<br />to<br />TOP</span>
+    </a>
 </div>
+
+<footer class="footer">
+    <div class="inner__footer">
+        <div class="container__sitemap">
+            <!-- gnav -->
+            <?php
+            $args = [
+                'menu' => 'Gnav', //管理画面で作成したメニューの名前
+                'menu_class' => 'wrap__pages', //メニューを構成するulタグのクラス名
+                'container' => false, //<ul>タグを囲んでいるdivタグを消除
+            ];
+            wp_nav_menu($args);
+            ?>
+            <!-- footer-menu -->
+            <?php
+            $args = [
+                'menu' => 'footer_nav', //管理画面で作成したメニューの名前
+                'menu_class' => 'wrap__rules', //メニューを構成するulタグのクラス名
+                'container' => false, //<ul>タグを囲んでいるdivタグを消除
+            ];
+            wp_nav_menu($args);
+            ?>
+        </div>
+    </div>
+</footer>
 
 <?php wp_footer(); ?> <!-- wordpress 管理bar表示するのため。必ず書いてください -->
 
