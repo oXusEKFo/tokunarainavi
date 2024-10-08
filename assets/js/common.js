dbfc0b014c548b26f08ba7af7e222fd5edf1a5a7
oXusEKFo <s23ryx5n@gmail.com>
2024-10-08 17:42:22 +0900

    header/footer.htmlおよびcssを作りました
    Topページからヘッダー、フッターを切り離して
    header.html
    header.css
    footer.html
    footer.css
    
    を作りました

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,34 +1,27 @@
 "use strict";
 
-// header ハンバーガー
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    const menuIcon = document.getElementById("menuIcon");
-
-    // メニューが開いているかどうかを確認し、アイコンを切り替え
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-        menuIcon.innerHTML = "&#9776;";  // ハンバーガーアイコンに戻す
-    } else {
-        menu.classList.add("active");
-        menuIcon.innerHTML = "X";  // クローズアイコンに変える
-    }
-}
+jQuery(function ($) {
+    // ハンバーガーメニュー
+    $('.menu').on('click', function () {
+        $('.menu__line').toggleClass('active');
+        $('.gnav').fadeToggle();
+    });
 
 
-// Topへ戻るボタン
-$(function () {
-  let top = $(".back-to-top");
-  top.hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      top.fadeIn();
-    } else {
-      top.fadeOut();
-    }
-  });
-  top.click(function () {
-    $("body, html").animate({ scrollTop: 0 }, 200, "swing");
-    return false;
-  });
+    // Topへ戻るボタン
+    $(function () {
+        let top = $(".back-to-top");
+        top.hide();
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                top.fadeIn();
+            } else {
+                top.fadeOut();
+            }
+        });
+        top.click(function () {
+            $("body, html").animate({ scrollTop: 0 }, 200, "swing");
+            return false;
+        });
+    });
 });
