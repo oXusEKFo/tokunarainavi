e4a9d0a4c2e9b93ffd8856933e32f776c0f4f3b7
baku-syoubai <mozhangmei@gmail.com>
2024-10-09 09:51:58 +0900

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -25,3 +25,27 @@ jQuery(function ($) {
         });
     });
 });
+
+/**
+ *
+ *検索ー 選び から
+ */
+function showSubMenu(button) {
+    const subMenus = document.querySelectorAll('.sub-menu');
+    const buttons = document.querySelectorAll('.option_button_search');
+    subMenus.forEach(menu => {
+        menu.style.display = 'none';
+    });
+    const targetId = button.getAttribute('data-target');
+    const targetMenu = document.getElementById(targetId);
+    if (targetMenu) {
+        targetMenu.style.display = 'block';
+    }
+    buttons.forEach(btn => {
+        if (btn !== button) {
+            btn.style.display = 'inline-block';
+        } else {
+            btn.style.display = 'none';
+        }
+    });
+}
