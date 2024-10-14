d47fe7e76868ba61990d89532a4c2bfd68f8e407
baku-syoubai <mozhangmei@gmail.com>
2024-10-14 21:51:08 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,59 +1,51 @@
 <footer>
     <!-- Topへ戻るボタン -->
-    <div class="back-to-top">
-        <a href="#">
-            <span>BACK<br />to<br />TOP</span>
-        </a>
-    </div>
+    <a href="#">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/TOPbtn.png" class="back-to-top" alt="Back to top" />
+    </a>
     <!-- フッター -->
-    <div class="inner__footer">
-        <div class="container__sitemap">
-            <ul class="wrap__items">
+    <div class="footer__container">
+        <div class="footer__links">
+            <ul>
                 <li>
-                    <a href="<?php echo home_url(); ?>/?s=" class="search__sitemap">
-                        条件検索
-                    </a>
+                    <a href="<?php echo home_url(); ?>/?s=" class="search__sitemap">条件検索</a>
                 </li>
                 <li>
-                    <a href="<?php echo home_url('/fushion'); ?>">
-                        徳島の習いごと調査
-                    </a>
+                    <a href="<?php echo home_url('/fushion'); ?>">徳島の習いごと調査</a>
                 </li>
                 <li>
-                    <a href="<?php echo home_url('/column'); ?>" class="column__sitemap">
-                        コラム
-                    </a>
+                    <a href="<?php echo home_url('/column'); ?>" class="column__sitemap">コラム</a>
                 </li>
                 <li>
-                    <a href="<?php echo home_url('/category/news'); ?>" class="news__sitemap">
-                        新着情報
-                    </a>
+                    <a href="<?php echo home_url('/category/news'); ?>" class="news__sitemap">新着情報</a>
                 </li>
                 <li>
-                    <a href="<?php echo home_url('/favor') ?>" class="favorite__sitemap">お気に入りリスト
-                    </a>
+                    <a href="<?php echo home_url('/favor') ?>" class="favorite__sitemap">お気に入りリスト</a>
                 </li>
                 <li>
-                    <a href="<?php echo home_url('/contact') ?>" class="input__sitemap">
-                        お問い合わせ
-                    </a>
+                    <a href="<?php echo home_url('/contact') ?>" class="input__sitemap">お問い合わせ</a>
                 </li>
             </ul>
-            <!-- footer-menu -->
+        </div>
+        <!-- footer-menu -->
+        <div class="footer__info">
             <?php
             $args = [
                 'menu' => 'footer_nav', //管理画面で作成したメニューの名前
-                'menu_class' => 'wrap__rules', //メニューを構成するulタグのクラス名
+                'menu_class' => '', //メニューを構成するulタグのクラス名
                 'container' => false, //<ul>タグを囲んでいるdivタグを消除
             ];
             wp_nav_menu($args);
             ?>
         </div>
-        <div class="copy-rights">
-            <p>&copy;2024&nbsp;QLIP&nbsp;とくしま習いごとナビ&#44;&nbsp;All&nbsp;rights&nbsp;reserved</p>
+        <div class="footer__illustration">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/birds.png" class="fade__in" alt="Illustration" />
         </div>
     </div>
 
+    <div class="footer__bottom">
+        <small>&copy;QLIP とくしま習いごとナビ</small>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
