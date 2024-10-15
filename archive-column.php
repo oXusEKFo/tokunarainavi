d11a719e8290c114f5cb9549b53affd50b5f8407
Haruto Isai <haruto133219@gmail.com>
2024-10-15 12:09:16 +0900

    Update column files
    デザインを修正しました
    
    以西

--- archive-column.php
+++ archive-column.php
@@ -7,7 +7,18 @@
                 <?php get_template_part('template-parts/breadcrumb'); ?>
             </div>
         </div>
-
+        <?php
+        $colurmn_terms = get_terms(['taxonomy' => 'column_type']);
+        ?>
+        <?php
+        if (!empty($colurmn_terms)): ?>
+            <?php foreach ($colurmn_terms as $column): ?>
+                <div>
+                    <a href="<?php echo get_term_link($column);
+                                ?>"><?= $column->name ?></a>
+                </div>
+            <?php endforeach; ?>
+        <?php endif ?>
         <!-- コラム一覧カード -->
         <div class="column__area">
             <div class="column__title">
@@ -16,24 +27,24 @@
             </div>
             <!-- スライダー ここから -->
             <div class="inner__column-area">
-                <div class="wrap__column-card">
-                    <?php
-                    $args = [
-                        'post_type' => 'column',
-                        // 'posts_per_page'     => 5,
-                        'orderby'        => 'date',
-                        'order'          => 'DESC',
-                    ];
-                    $column_query = new WP_query($args); ?>
-                    <?php
-                    if ($column_query->have_posts()):
-                    ?>
-                        <?php while ($column_query->have_posts()): $column_query->the_post(); ?>
+                <?php
+                $args = [
+                    'post_type' => 'column',
+                    // 'posts_per_page'     => 5,
+                    'orderby'        => 'date',
+                    'order'          => 'DESC',
+                ];
+                $column_query = new WP_query($args); ?>
+                <?php
+                if ($column_query->have_posts()):
+                ?>
+                    <?php while ($column_query->have_posts()): $column_query->the_post(); ?>
+                        <div class="wrap__column-card">
                             <?php get_template_part('template-parts/loop', 'column'); ?>
-                </div>
-            <?php endwhile; ?>
-            <!-- <?php wp_reset_postdata(); ?> -->
-        <?php endif ?>
+                        </div>
+                    <?php endwhile; ?>
+                    <!-- <?php wp_reset_postdata(); ?> -->
+                <?php endif ?>
             </div>
         </div>
         <!-- コラムカードここまで -->
