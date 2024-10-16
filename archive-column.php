69661a3cfdbe9bdbd631821c0442ce925dfdaea1
baku-syoubai <mozhangmei@gmail.com>
2024-10-16 10:35:15 +0900

    メニューを追加します
    メニューを追加します

--- archive-column.php
+++ archive-column.php
@@ -1,65 +1,76 @@
 <?php get_header(); ?>
 <main>
-    <!--  -->
+
     <div class="inner__main">
         <div class="container__breadCrumb">
             <div class="breadCrumb">
                 <?php get_template_part('template-parts/breadcrumb'); ?>
             </div>
         </div>
-        <?php
-        $colurmn_terms = get_terms(['taxonomy' => 'column_type']);
-        ?>
-        <?php
-        if (!empty($colurmn_terms)): ?>
-            <?php foreach ($colurmn_terms as $column): ?>
-                <div>
-                    <a href="<?php echo get_term_link($column);
-                                ?>"><?= $column->name ?></a>
+
+        <!-- メニュー -->
+        <div class="archive">
+            <div class="archive_category">
+                <ul class="archive_list">
+                    <?php
+                    $all_link = home_url('/column');
+                    echo '<li><a href="' . $all_link . '">All</a></li>';
+
+                    $terms = get_terms(array(
+                        'taxonomy' => 'column_type',
+                        'hide_empty' => false,
+                    ));
+
+                    if (!empty($terms) && !is_wp_error($terms)) {
+
+                        foreach ($terms as $term) {
+                            echo '<li><a href="' . esc_url(get_term_link($term)) . '">' . esc_html($term->name) . '</a></li>';
+                        }
+                    }
+                    ?>
+                </ul>
+            </div>
+
+            <!-- コラム一覧カード -->
+            <div class="column__area">
+                <div class="column__title">
+                    <h1>COLUMN</h1>
+                    <p>コラム</p>
                 </div>
-            <?php endforeach; ?>
-        <?php endif ?>
-        <!-- コラム一覧カード -->
-        <div class="column__area">
-            <div class="column__title">
-                <h1>COLUMN</h1>
-                <p>コラム</p>
+                <!-- スライダー ここから -->
+                <div class="inner__column-area">
+                    <?php
+                    $args = [
+                        'post_type' => 'column',
+                        // 'posts_per_page'     => 5,
+                        'orderby'        => 'date',
+                        'order'          => 'DESC',
+                    ];
+                    $column_query = new WP_query($args); ?>
+                    <?php
+                    if ($column_query->have_posts()):
+                    ?>
+                        <?php while ($column_query->have_posts()): $column_query->the_post(); ?>
+                            <div class="wrap__column-card">
+                                <?php get_template_part('template-parts/loop', 'column'); ?>
+                            </div>
+                        <?php endwhile; ?>
+                        <!-- <?php wp_reset_postdata(); ?> -->
+                    <?php endif ?>
+                </div>
             </div>
-            <!-- スライダー ここから -->
-            <div class="inner__column-area">
-                <?php
-                $args = [
-                    'post_type' => 'column',
-                    // 'posts_per_page'     => 5,
-                    'orderby'        => 'date',
-                    'order'          => 'DESC',
-                ];
-                $column_query = new WP_query($args); ?>
-                <?php
-                if ($column_query->have_posts()):
-                ?>
-                    <?php while ($column_query->have_posts()): $column_query->the_post(); ?>
-                        <div class="wrap__column-card">
-                            <?php get_template_part('template-parts/loop', 'column'); ?>
+            <!-- コラムカードここまで -->
+
+            <div class="column__footer">
+                <div class="container__page-num">
+                    <?php if (function_exists('wp_pagenavi')):  ?>
+                        <div class="column_page-num">
+                            <?php wp_pagenavi(); ?>
                         </div>
-                    <?php endwhile; ?>
-                    <!-- <?php wp_reset_postdata(); ?> -->
-                <?php endif ?>
+                    <?php endif;  ?>
+                </div>
             </div>
         </div>
-        <!-- コラムカードここまで -->
-
-        <div class="column__footer">
-            <div class="container__page-num">
-
-                <?php if (function_exists('wp_pagenavi')):  ?>
-                    <div class="column_page-num">
-                        <?php wp_pagenavi(); ?>
-                    </div>
-                <?php endif;  ?>
-            </div>
-        </div>
-    </div>
 </main>
 
 <?php get_footer(); ?>
--- taxonomy-column_type.php
+++ taxonomy-column_type.php
@@ -11,41 +11,60 @@ $column = get_term_by('slug', $column_slug, 'column_type');
                 <?php get_template_part('template-parts/breadcrumb'); ?>
             </div>
         </div>
+        <!-- メニュー -->
+        <div class="archive">
+            <div class="archive_category">
+                <ul class="archive_list">
+                    <?php
+                    $all_link = home_url('/column');
+                    echo '<li><a href="' . $all_link . '">All</a></li>';
 
-        <!-- コラム一覧カード -->
-        <div class="inner__column">
-            <div class="title__column">
-                <h1>COLUMN</h1>
-                <p><?php single_term_title('');
-                    ?></p>
+                    $terms = get_terms(array(
+                        'taxonomy' => 'column_type',
+                        'hide_empty' => false,
+                    ));
+
+                    if (!empty($terms) && !is_wp_error($terms)) {
+
+                        foreach ($terms as $term) {
+                            echo '<li><a href="' . esc_url(get_term_link($term)) . '">' . esc_html($term->name) . '</a></li>';
+                        }
+                    }
+                    ?>
+                </ul>
             </div>
-            <!-- スライダー ここから -->
-            <div class="slider">
-                <div class="auto-slider">
+
+            <!-- コラム一覧カード -->
+            <div class="column__area">
+                <div class="column__title">
+                    <h1>COLUMN</h1>
+                    <p><?php single_term_title('');
+                        ?></p>
+                </div>
+                <!-- スライダー ここから -->
+                <div class="inner__column-area">
                     <?php
                     if (have_posts()):
                     ?>
-                        <div class="inner__column-area">
-                            <?php while (have_posts()): the_post(); ?>
+                        <?php while (have_posts()): the_post(); ?>
+                            <div class="wrap__column-card">
                                 <?php get_template_part('template-parts/loop', 'column'); ?>
-                            <?php endwhile; ?>
-                            <?php wp_reset_postdata(); ?>
-                        </div>
+                            </div>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>
                     <?php endif ?>
                 </div>
             </div>
-        </div>
-        <div class="column__footer">
-            <div class="container__page-num">
-
-                <?php if (function_exists('wp_pagenavi')):  ?>
-                    <div class="column_page-num">
-                        <?php wp_pagenavi(); ?>
-                    </div>
-                <?php endif;  ?>
+            <div class="column__footer">
+                <div class="container__page-num">
+                    <?php if (function_exists('wp_pagenavi')):  ?>
+                        <div class="column_page-num">
+                            <?php wp_pagenavi(); ?>
+                        </div>
+                    <?php endif;  ?>
+                </div>
             </div>
         </div>
-    </div>
 </main>
 
 <?php get_footer(); ?>
