91b981d81593c41f572be4e76162004886dbf659
oXusEKFo <s23ryx5n@gmail.com>
2024-12-13 11:44:51 +0900

    Googleタグマネージャー設置
    Googleタグマネージャー設置

--- header.php
+++ header.php
@@ -15,6 +15,25 @@ if (!is_user_logged_in() && IS_DEV == true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=yes">
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PK4HP84D');
+    </script>
+    <!-- End Google Tag Manager -->
+
     <?php
     $description = "";
     $og_title = "";
@@ -94,6 +113,12 @@ if (!is_user_logged_in() && IS_DEV == true) {
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PK4HP84D"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <header>
         <div class="inner__header">
             <nav class="gnav">
