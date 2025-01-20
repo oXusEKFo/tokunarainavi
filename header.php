87928911e0ba2af3fd3f4bdbe2202ffa85bcaac9
oXusEKFo <s23ryx5n@gmail.com>
2025-01-20 14:03:33 +0900

    og title内容変更
    SNSでページリンクを共有する際に表示されるタイトルがわかりにくいので 変更を加えました

--- header.php
+++ header.php
@@ -46,10 +46,10 @@ if (!is_user_logged_in() && IS_DEV == true) {
         $og_url = home_url();
     } elseif (is_page('favor')) {
         $description = "お気に入り登録した教室の一覧です。";
-        $og_title = "|お気に入りページ";
+        $og_title = "|お気に入り";
     } elseif (is_page('contact')) {
         $description = "お問い合わせと掲載申し込みのページです。";
-        $og_title = "|入力フォーム";
+        $og_title = "|お問い合わせ、掲載申し込み";
     } elseif (is_page('confirm')) {
         $description = "内容をご確認の上、問題がなければ送信ボタンを押してください。修正が必要な場合は、戻るボタンで編集が可能です。";
         $og_title = "|送信内容確認";
@@ -58,30 +58,30 @@ if (!is_user_logged_in() && IS_DEV == true) {
         $og_title = "|送信完了";
     } elseif (is_search()) {
         $description = "徳島県内の子供向け習い事を、エリア、年齢、ジャンルなどから検索できます。";
-        $og_title = "|SEARCH";
+        $og_title = "|習い事検索";
     } elseif (is_singular('classroom')) {
-        $description = "習いごと教室の詳細です。";
-        $og_title = "|結果詳細";
+        $description = "習い事教室の詳細です。";
+        $og_title = "|習い事教室詳細";
     } elseif (is_page('fushion')) {
         $description = "徳島の習い事事情についてのアンケート結果です。";
-        $og_title = "|習い事事情ページ";
+        $og_title = "|徳島の習い事事情（アンケート結果）";
     } elseif (is_post_type_archive('column') || is_tax('column_type') || is_singular('column')) {
-        $description = "習いごと教室へのインタビューや体験レビューです。";
+        $description = "習い事教室へのインタビューや体験レビューです。";
         if (is_singular('column')) {
-            $og_title = "|COLUMN詳細";
+            $og_title = "|コラム詳細";
         } else {
-            $og_title = "|COLUMN一覧";
+            $og_title = "|コラム一覧";
         }
     } elseif (is_page('infos') || is_category() || is_single()) {
         $description = "お知らせ、更新情報、イベント情報です。";
         if (is_single()) {
-            $og_title = "|NEWS詳細";
+            $og_title = "|ニュース詳細";
         } else {
-            $og_title = "|NEWS一覧";
+            $og_title = "|ニュース一覧";
         }
     } elseif (is_page('about')) {
         $description = "とくしま習いごとナビの説明です。";
-        $og_title = "|このサイトについて";
+        $og_title = "|徳島習いごとナビとは？";
     } elseif (is_page('service')) {
         $description = "とくしま習いごとナビの利用規約です。";
         $og_title = "|利用規約";
